dbb6691aa8087a3cff992ac17a0195a59dbacf3a
Richard Rosario <tech2js@gmail.com>
2020-07-09 21:33:08 +0800

    adding element targets and  initial js classes

---
+++ app.js
@@ -0,0 +1,22 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverly = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContant = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+let cart = [];
+// getting products
+class Products {}
+// display products
+class UI {}
+// localstorage
+class Storage {}
+// event listeners
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
